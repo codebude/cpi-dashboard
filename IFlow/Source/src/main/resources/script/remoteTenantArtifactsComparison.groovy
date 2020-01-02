3312f303825a486ea00559d9d14ca2168c8c6b90
Raffael Herrmann <netblognet@googlemail.com>
2020-01-02 14:27:00 +0100

    Bugfix in artifact matching rule
    Name and id were mixed up, which lead to mismatches.

--- IFlow/Source/src/main/resources/script/remoteTenantArtifactsComparison.groovy
+++ IFlow/Source/src/main/resources/script/remoteTenantArtifactsComparison.groovy
@@ -14,109 +14,109 @@ Message processData(Message message) {
 	def systems = getSystems(message)
 	def orderOfSystems = message.getProperty("remoteTenantsOrder")
 
-  def i = 0
+	def i = 0
 	def result = [systems:[:],artifacts:[]]
-  def artifacts = [:]
+	def artifacts = [:]
 	systems.each{ system ->
-    result.systems << ["${i}":system.hostname]
-    artifacts = addDesigntimeArtifacts(system,artifacts,i)
-    artifacts = addRuntimeArtifacts(system,artifacts,i)
-    i++
+		result.systems << ["${i}":system.hostname]
+		artifacts = addDesigntimeArtifacts(system,artifacts,i)
+		artifacts = addRuntimeArtifacts(system,artifacts,i)
+		i++
 	}
 
-  // Improve artifacts - set missing versions and evaluate issues
-  artifacts.each{artifact ->
+	// Improve artifacts - set missing versions and evaluate issues
+	artifacts.each{artifact ->
 		def findings = ""
 		artifacts[artifact.key].Severity = "OK"
-    0.upto(i-1, {
-      if(!artifacts[artifact.key].get("VersionDesigntime").containsKey("${it}")){
-        artifacts[artifact.key].VersionDesigntime << ["${it}":'']
-      }
-      if(!artifacts[artifact.key].get("VersionRuntime").containsKey("${it}")){
-        artifacts[artifact.key].VersionRuntime << ["${it}":'']
-      }
-      if(!artifacts[artifact.key].get("DesigntimePackage").containsKey("${it}")){
-        artifacts[artifact.key].DesigntimePackage << ["${it}":'']
-      }
-
-			////// Evaluate issues
-			//// Evaluate general issues on one tenant
-			// Check if the artifact is deployed on the runtime but not available on the designtime of the same tenant
-			if((artifacts[artifact.key].get('VersionDesigntime').get("${it}") == '') && !(artifacts[artifact.key].get('VersionRuntime').get("${it}") == '')) {
-				 findings += "Available on Runtime [${it}] but not on Designtime [${it}]|"
-				 artifacts[artifact.key].Severity = "CRITICAL"
-			// Check if the versions on design and runtime are different on the same tenant (only if available on runtime)
-			} else if((artifacts[artifact.key].get('VersionRuntime').get("${it}") != '') && ((artifacts[artifact.key].get('VersionDesigntime').get("${it}") != artifacts[artifact.key].get('VersionRuntime').get("${it}")))) {
-				 findings += "Different Version on Runtime and Designtime [${it}]|"
-				 if(artifacts[artifact.key].Severity == "OK") {
-				 	artifacts[artifact.key].Severity = "WARNING"
-				 }			// Check if the artifact is available on the Designtime of the tenant (in case it is not available on all tenants)
-			} else if((artifacts[artifact.key].get('VersionDesigntime').get("${it}") == '') && (orderOfSystems != 'UP') && (orderOfSystems != 'DOWN')) {
-				 findings += "Not available on Designtime [${it}]|"
-				 if(artifacts[artifact.key].Severity == "OK") {
-					 artifacts[artifact.key].Severity = "WARNING"
-				 }
+		0.upto(i-1, {
+			if(!artifacts[artifact.key].get("VersionDesigntime").containsKey("${it}")){
+				artifacts[artifact.key].VersionDesigntime << ["${it}":'']
+			}
+			if(!artifacts[artifact.key].get("VersionRuntime").containsKey("${it}")){
+				artifacts[artifact.key].VersionRuntime << ["${it}":'']
+			}
+			if(!artifacts[artifact.key].get("DesigntimePackage").containsKey("${it}")){
+				artifacts[artifact.key].DesigntimePackage << ["${it}":'']
 			}
 
-			//// Evaluate issues based on multiple tenants (only applicable if external parameter 'remoteTenantsOrder' is set)
-			if (orderOfSystems == "DOWN") {
-				if((it > 0) && (it <= i)) {
-					if (artifacts[artifact.key].get('VersionDesigntime').get("${it-1}")?.trim()) {
-						//// Not necessary: Check if a newer version of an artifact is deployed on an upper tenant and if not, check if a newer version of an artifact is deployed on an upper tenant
-						// if (!(artifacts[artifact.key].get('VersionRuntime').get("${it-1}") == '') && (artifacts[artifact.key].get('VersionRuntime').get("${it}") == '')) {
-						// 	 findings += "Artifact is available on runtime [${it-1}] but not on [${it}]|"
-						// 	 artifacts[artifact.key].Severity = "CRITICAL"
-						// } else if ((artifacts[artifact.key].get('VersionRuntime').get("${it-1}") != '') && (artifacts[artifact.key].get('VersionRuntime').get("${it-1}") != artifacts[artifact.key].get('VersionRuntime').get("${it}")) && !(artifacts[artifact.key].get('VersionRuntime').get("${it-1}") == getLatestVersion(['' + artifacts[artifact.key].get('VersionRuntime').get("${it}"), '' + artifacts[artifact.key].get('VersionRuntime').get("${it-1}")]))) {
-						// 	findings += "Newer version on runtime of [${it-1}] as on [${it}]|"
-						// 	artifacts[artifact.key].Severity = "CRITICAL"
-						// }
+				////// Evaluate issues
+				//// Evaluate general issues on one tenant
+				// Check if the artifact is deployed on the runtime but not available on the designtime of the same tenant
+				if((artifacts[artifact.key].get('VersionDesigntime').get("${it}") == '') && !(artifacts[artifact.key].get('VersionRuntime').get("${it}") == '')) {
+					 findings += "Available on Runtime [${it}] but not on Designtime [${it}]|"
+					 artifacts[artifact.key].Severity = "CRITICAL"
+				// Check if the versions on design and runtime are different on the same tenant (only if available on runtime)
+				} else if((artifacts[artifact.key].get('VersionRuntime').get("${it}") != '') && ((artifacts[artifact.key].get('VersionDesigntime').get("${it}") != artifacts[artifact.key].get('VersionRuntime').get("${it}")))) {
+					 findings += "Different Version on Runtime and Designtime [${it}]|"
+					 if(artifacts[artifact.key].Severity == "OK") {
+						artifacts[artifact.key].Severity = "WARNING"
+					 }			// Check if the artifact is available on the Designtime of the tenant (in case it is not available on all tenants)
+				} else if((artifacts[artifact.key].get('VersionDesigntime').get("${it}") == '') && (orderOfSystems != 'UP') && (orderOfSystems != 'DOWN')) {
+					 findings += "Not available on Designtime [${it}]|"
+					 if(artifacts[artifact.key].Severity == "OK") {
+						 artifacts[artifact.key].Severity = "WARNING"
+					 }
+				}
 
-						// Check if the artifact is not available on a lower tenant and if not, check if a newer version of an artifact is available on an upper tenant
-						if (!(artifacts[artifact.key].get('VersionDesigntime').get("${it-1}") == '') && (artifacts[artifact.key].get('VersionDesigntime').get("${it}") == '')) {
-							 findings += "Artifact is available on Designtime [${it-1}] but not on [${it}]|"
-							 artifacts[artifact.key].Severity = "CRITICAL"
-						} else if ((artifacts[artifact.key].get('VersionDesigntime').get("${it-1}") != '') && (artifacts[artifact.key].get('VersionDesigntime').get("${it-1}") != artifacts[artifact.key].get('VersionDesigntime').get("${it}")) && !(artifacts[artifact.key].get('VersionDesigntime').get("${it-1}") == getLatestVersion(['' + artifacts[artifact.key].get('VersionDesigntime').get("${it}"), '' + artifacts[artifact.key].get('VersionDesigntime').get("${it-1}")]))) {
-							 findings += "Newer version on Designtime of [${it-1}] as on [${it}]|"
-							 artifacts[artifact.key].Severity = "CRITICAL"
+				//// Evaluate issues based on multiple tenants (only applicable if external parameter 'remoteTenantsOrder' is set)
+				if (orderOfSystems == "DOWN") {
+					if((it > 0) && (it <= i)) {
+						if (artifacts[artifact.key].get('VersionDesigntime').get("${it-1}")?.trim()) {
+							//// Not necessary: Check if a newer version of an artifact is deployed on an upper tenant and if not, check if a newer version of an artifact is deployed on an upper tenant
+							// if (!(artifacts[artifact.key].get('VersionRuntime').get("${it-1}") == '') && (artifacts[artifact.key].get('VersionRuntime').get("${it}") == '')) {
+							// 	 findings += "Artifact is available on runtime [${it-1}] but not on [${it}]|"
+							// 	 artifacts[artifact.key].Severity = "CRITICAL"
+							// } else if ((artifacts[artifact.key].get('VersionRuntime').get("${it-1}") != '') && (artifacts[artifact.key].get('VersionRuntime').get("${it-1}") != artifacts[artifact.key].get('VersionRuntime').get("${it}")) && !(artifacts[artifact.key].get('VersionRuntime').get("${it-1}") == getLatestVersion(['' + artifacts[artifact.key].get('VersionRuntime').get("${it}"), '' + artifacts[artifact.key].get('VersionRuntime').get("${it-1}")]))) {
+							// 	findings += "Newer version on runtime of [${it-1}] as on [${it}]|"
+							// 	artifacts[artifact.key].Severity = "CRITICAL"
+							// }
+
+							// Check if the artifact is not available on a lower tenant and if not, check if a newer version of an artifact is available on an upper tenant
+							if (!(artifacts[artifact.key].get('VersionDesigntime').get("${it-1}") == '') && (artifacts[artifact.key].get('VersionDesigntime').get("${it}") == '')) {
+								 findings += "Artifact is available on Designtime [${it-1}] but not on [${it}]|"
+								 artifacts[artifact.key].Severity = "CRITICAL"
+							} else if ((artifacts[artifact.key].get('VersionDesigntime').get("${it-1}") != '') && (artifacts[artifact.key].get('VersionDesigntime').get("${it-1}") != artifacts[artifact.key].get('VersionDesigntime').get("${it}")) && !(artifacts[artifact.key].get('VersionDesigntime').get("${it-1}") == getLatestVersion(['' + artifacts[artifact.key].get('VersionDesigntime').get("${it}"), '' + artifacts[artifact.key].get('VersionDesigntime').get("${it-1}")]))) {
+								 findings += "Newer version on Designtime of [${it-1}] as on [${it}]|"
+								 artifacts[artifact.key].Severity = "CRITICAL"
+							}
+						}
+					}
+				} else if (orderOfSystems == "UP") {
+					if (it < systems.size() - 1) {
+						def j = (systems.size() - 1) - it
+						if (artifacts[artifact.key].get('VersionDesigntime').get("${j}")?.trim()) {
+							//// Not necessary: Check if a newer version of an artifact is deployed on an upper tenant and if not, check if a newer version of an artifact is deployed on an upper tenant
+							// if (!(artifacts[artifact.key].get('VersionRuntime').get("${j}") == '') && (artifacts[artifact.key].get('VersionRuntime').get("${j-1}") == '')) {
+							// 	 findings += "Artifact is available on runtime [${j}] but not on [${j-1}]|"
+							// 	 artifacts[artifact.key].Severity = "CRITICAL"
+							// } else if ((artifacts[artifact.key].get('VersionRuntime').get("${j}") != '') && (artifacts[artifact.key].get('VersionRuntime').get("${j}") != artifacts[artifact.key].get('VersionRuntime').get("${j-1}")) && !(artifacts[artifact.key].get('VersionRuntime').get("${j}") == getLatestVersion(['' + artifacts[artifact.key].get('VersionRuntime').get("${j}"), '' + artifacts[artifact.key].get('VersionRuntime').get("${j-1}")]))) {
+							// 	findings += "Newer version on runtime of [${j}] as on [${j-1}]|"
+							// 	artifacts[artifact.key].Severity = "CRITICAL"
+							// }
+
+							// Check if the artifact is not available on a lower tenant and if not, check if a newer version of an artifact is available on an upper tenant
+							if (!(artifacts[artifact.key].get('VersionDesigntime').get("${j}") == '') && (artifacts[artifact.key].get('VersionDesigntime').get("${j-1}") == '')) {
+								 findings += "Artifact is available on Designtime [${j}] but not on [${j-1}]|"
+								 artifacts[artifact.key].Severity = "CRITICAL"
+							} else if ((artifacts[artifact.key].get('VersionDesigntime').get("${j}") != '') && (artifacts[artifact.key].get('VersionDesigntime').get("${j}") != artifacts[artifact.key].get('VersionDesigntime').get("${j-1}")) && !(artifacts[artifact.key].get('VersionDesigntime').get("${j}") == getLatestVersion(['' + artifacts[artifact.key].get('VersionDesigntime').get("${j}"), '' + artifacts[artifact.key].get('VersionDesigntime').get("${j-1}")]))) {
+								 findings += "Newer version on Designtime of [${j}] as on [${j-1}]|"
+								 artifacts[artifact.key].Severity = "CRITICAL"
+							}
 						}
 					}
 				}
-			} else if (orderOfSystems == "UP") {
-				if (it < systems.size() - 1) {
-					def j = (systems.size() - 1) - it
-					if (artifacts[artifact.key].get('VersionDesigntime').get("${j}")?.trim()) {
-						//// Not necessary: Check if a newer version of an artifact is deployed on an upper tenant and if not, check if a newer version of an artifact is deployed on an upper tenant
-						// if (!(artifacts[artifact.key].get('VersionRuntime').get("${j}") == '') && (artifacts[artifact.key].get('VersionRuntime').get("${j-1}") == '')) {
-						// 	 findings += "Artifact is available on runtime [${j}] but not on [${j-1}]|"
-						// 	 artifacts[artifact.key].Severity = "CRITICAL"
-						// } else if ((artifacts[artifact.key].get('VersionRuntime').get("${j}") != '') && (artifacts[artifact.key].get('VersionRuntime').get("${j}") != artifacts[artifact.key].get('VersionRuntime').get("${j-1}")) && !(artifacts[artifact.key].get('VersionRuntime').get("${j}") == getLatestVersion(['' + artifacts[artifact.key].get('VersionRuntime').get("${j}"), '' + artifacts[artifact.key].get('VersionRuntime').get("${j-1}")]))) {
-						// 	findings += "Newer version on runtime of [${j}] as on [${j-1}]|"
-						// 	artifacts[artifact.key].Severity = "CRITICAL"
-						// }
-
-						// Check if the artifact is not available on a lower tenant and if not, check if a newer version of an artifact is available on an upper tenant
-						if (!(artifacts[artifact.key].get('VersionDesigntime').get("${j}") == '') && (artifacts[artifact.key].get('VersionDesigntime').get("${j-1}") == '')) {
-							 findings += "Artifact is available on Designtime [${j}] but not on [${j-1}]|"
-							 artifacts[artifact.key].Severity = "CRITICAL"
-						} else if ((artifacts[artifact.key].get('VersionDesigntime').get("${j}") != '') && (artifacts[artifact.key].get('VersionDesigntime').get("${j}") != artifacts[artifact.key].get('VersionDesigntime').get("${j-1}")) && !(artifacts[artifact.key].get('VersionDesigntime').get("${j}") == getLatestVersion(['' + artifacts[artifact.key].get('VersionDesigntime').get("${j}"), '' + artifacts[artifact.key].get('VersionDesigntime').get("${j-1}")]))) {
-							 findings += "Newer version on Designtime of [${j}] as on [${j-1}]|"
-							 artifacts[artifact.key].Severity = "CRITICAL"
-						}
-					}
-				}
-			}
-    })
+			})
 		if(findings.length() > 0) {
 			artifacts[artifact.key].Remarks = findings.substring(0, findings.length() - 1)
 		}
-  }
+	}
 
-  result.artifacts = artifacts
+	result.artifacts = artifacts
 
-  def body = JsonOutput.toJson(result)
-  message.setBody(body)
-  message.setHeader('Content-Type', 'application/json')
-  return message
+	def body = JsonOutput.toJson(result)
+	message.setBody(body)
+	message.setHeader('Content-Type', 'application/json')
+	return message
 }
 
 private addDesigntimeArtifacts(def system, def artifacts, def i){
@@ -131,28 +131,26 @@ private addDesigntimeArtifacts(def system, def artifacts, def i){
 	pkgJson.d.results.each{
 		def pkgContentUrl = "https://${system.hostname}/itspaces/odata/1.0/workspace.svc/ContentEntities.ContentPackages('${it.TechnicalName}')/Artifacts?\$format=json"
 		def pkgContentList = pkgContentUrl.toURL().getText([requestProperties:[Authorization:system.credential.basicAuth,"Accept":"application/json"]])
-    def packageDisplayname = "${it.TechnicalName}"
+		def packageDisplayname = "${it.TechnicalName}"
 
-		def iFlowList = []
 		def json = new JsonSlurper().parseText(pkgContentList)
-		def artifactsOut = artifacts
-    json.d.results.each{
-        if (artifactsOut.containsKey(it.Name)){
-            artifactsOut[it.Name].VersionDesigntime << ["${i}":it.Version]
-            artifactsOut[it.Name].VersionRuntime << ["${i}":'']
-            artifactsOut[it.Name].DesigntimePackage << ["${i}":packageDisplayname]
-        } else {
-            artifactsOut[it.Name]= [
-                Id:it.Name,
-                Name:it.DisplayName,
-                DesigntimePackage:["${i}":packageDisplayname],
-                VersionRuntime:["${i}":''],
-                VersionDesigntime: ["${i}":it.Version],
-                Type:(it.Type=="IFlow"?"INTEGRATION_FLOW":(it.Type=="ValueMapping"?"VALUE_MAPPING":it.Type)),
-								Remarks:''
-            ]
-        }
-      }
+		json.d.results.each{
+			if (artifacts.containsKey(it.Name)){
+				artifacts[it.Name].VersionDesigntime << ["${i}":it.Version]
+				artifacts[it.Name].VersionRuntime << ["${i}":'']
+				artifacts[it.Name].DesigntimePackage << ["${i}":packageDisplayname]
+			} else {
+				artifacts[it.Name]= [
+					Id:it.Name,
+					Name:it.DisplayName,
+					DesigntimePackage:["${i}":packageDisplayname],
+					VersionRuntime:["${i}":''],
+					VersionDesigntime: ["${i}":it.Version],
+					Type:(it.Type=="IFlow"?"INTEGRATION_FLOW":(it.Type=="ValueMapping"?"VALUE_MAPPING":it.Type)),
+									Remarks:''
+				]
+			}
+		}
     }
     return artifacts
 }
@@ -166,7 +164,7 @@ private addRuntimeArtifacts(def system, def artifacts, def i){
 	def runtimeArtifactsList = runtimeArtifactsUrl.toURL().getText([requestProperties:[Authorization:system.credential.basicAuth,"Accept":"application/json"]])
 	def runtimeArtifactsJson = new JsonSlurper().parseText(runtimeArtifactsList)
 	runtimeArtifactsJson.d.results.each{
-    if (artifacts.containsKey(it.Name)){
+    if (artifacts.containsKey(it.Id)){
         artifacts[it.Id].VersionRuntime << ["${i}":it.Version]
     } else {
         artifacts[it.Id]= [
