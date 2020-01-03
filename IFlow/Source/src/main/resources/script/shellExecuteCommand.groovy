54fd19da9d314d94a66b10a6fc55077f5cb26021
Raffael Herrmann <netblognet@googlemail.com>
2020-01-03 07:23:06 +0100

    Added shell cmd history and improved unkwown command handling

--- IFlow/Source/src/main/resources/script/shellExecuteCommand.groovy
+++ IFlow/Source/src/main/resources/script/shellExecuteCommand.groovy
@@ -8,41 +8,44 @@ import groovy.json.*
 import org.fusesource.jansi.*
 
 Message processData(Message message) {
-  
-    //Get command and build SessionFactory/Shell
-    String command = message.getProperty("shellCommand")
-    ByteArrayOutputStream out = new ByteArrayOutputStream()
-    ByteArrayOutputStream err = new ByteArrayOutputStream()
-    BundleContext context = FrameworkUtil.getBundle(Message.class).bundleContext
-    SessionFactory sessionFactory = (SessionFactory) context.getService(context.getServiceReference(SessionFactory.class))
+  	
+	def result = []
+	try {
+		//Get command and build SessionFactory/Shell
+		String command = message.getProperty("shellCommand")
+		ByteArrayOutputStream out = new ByteArrayOutputStream()
+		ByteArrayOutputStream err = new ByteArrayOutputStream()
+		BundleContext context = FrameworkUtil.getBundle(Message.class).bundleContext
+		SessionFactory sessionFactory = (SessionFactory) context.getService(context.getServiceReference(SessionFactory.class))
 
-	def hOut = new HtmlAnsiOutputStream(out)
-	def hErr = new HtmlAnsiOutputStream(err)
-	Session session = sessionFactory.create(new ByteArrayInputStream(), new GroovyPrintStream(hOut, true, "UTF-8"), new GroovyPrintStream(hErr, true, "UTF-8"))
-    
-    //Execute command and parse response
-    session.execute(command)
-    def cmdResult = out.toString()
-    def errResult = err.toString()
+		def hOut = new HtmlAnsiOutputStream(out)
+		def hErr = new HtmlAnsiOutputStream(err)
+		Session session = sessionFactory.create(new ByteArrayInputStream(), new GroovyPrintStream(hOut, true, "UTF-8"), new GroovyPrintStream(hErr, true, "UTF-8"))
+		
+		//Execute command and parse response
+		session.execute(command)
+		def cmdResult = out.toString()
+		def errResult = err.toString()
 
-    //Remove ANSI escapes codes which may be uncovered by Jansi from responses
-	cmdResult = decolorizeString(cmdResult)
-	errResult = decolorizeString(errResult)
-        
-    //Format the output
-	cmdResult = cmdResult.replace("\t", "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;").replace("\r\n", "\n").replace("\n", "<br/>").replace(" ", "&nbsp;")
-    errResult = "<span style='color:red;'>"+errResult.replace("\t", "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;").replace("\r\n", "\n").replace("\n", "<br/>").replace(" ", "&nbsp;")+"</span>"
+		//Remove ANSI escapes codes which may be uncovered by Jansi from responses
+		cmdResult = decolorizeString(cmdResult)
+		errResult = decolorizeString(errResult)
+			
+		//Format the output
+		cmdResult = cmdResult.replace("\t", "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;").replace("\r\n", "\n").replace("\n", "<br/>").replace(" ", "&nbsp;")
+		errResult = "<span style='color:red;'>"+errResult.replace("\t", "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;").replace("\r\n", "\n").replace("\n", "<br/>").replace(" ", "&nbsp;")+"</span>"
+		result = [
+			outStream: cmdResult,
+			errStream: errResult
+		]
+		session.close()
+	} catch (Exception ex) { }
 	
-	//Build datatype and set JSON header
-	def result = [
-		outStream: cmdResult,
-		errStream: errResult
-	]
+	//Build datatype and set JSON header	 
 	def response = JsonOutput.toJson(result)
 	message.setBody(response)
 	message.setHeader('Content-Type', 'application/json')
-   
-    session.close()
+       
     return message
 }
 
